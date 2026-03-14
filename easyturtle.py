601d9ea0efd6cccf50669592a74e6be314e1f231
OTISKI <otis.veraghtert@leerling.vtiz.be>
2026-03-14 14:41:50 +0100

    basicTurtleCommands

--- easyturtle.py
+++ easyturtle.py
@@ -1,2 +1,20 @@
-def hello(text):
-    print(text)
+import turtle
+
+t = turtle.Pen()
+
+def circle(col, size):
+    t.color(col)
+    t.begin_fill()
+    t.circle(size)
+    t.end_fill()
+
+
+def square(col, size):
+    t.color(col)
+    t.begin_fill()
+    for i in range(4):
+        t.fd(size)
+        t.rt(90)
+    t.end_fill()
+def done():
+    turtle.done()
