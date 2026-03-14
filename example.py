e348321a8dd030656c181442295064adf73db75f
OTISKI <otis.veraghtert@gmail.com>
2026-03-14 21:20:57 +0100

    betterChronoOrder

--- example.py
+++ example.py
@@ -4,8 +4,8 @@ import easyturtle as et
 #sets background color
 et.bgcolor("black")
 
-et.patternCircle(200)
-"""
+
+
 #draws a square
 et.square("red", 100)
 
@@ -15,14 +15,14 @@ et.circle("green", 90)
 #draws a triangle
 et.triangle("blue", 75)
 
-#draws a circle pattern
+#draws a circle changing color
 et.patternCircle(200)
 
+#draws a circle pattern
+et.patternCircle(200)
 
 #clears and resets the screen
 et.clear()
 
 #keeps the window open
 et.done()
-
-"""
